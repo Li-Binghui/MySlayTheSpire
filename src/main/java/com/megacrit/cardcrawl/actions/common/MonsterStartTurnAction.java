dba0d33c99e8dfb583fc113b5424ca832e1ba901
Li Binghui <1119604962@qq.com>
2025-10-03 18:08:09 +0800

    fix: 修复心脏施加状态的bug，修复怪物回合开始时计算错误的bug

--- src/main/java/com/megacrit/cardcrawl/actions/common/MonsterStartTurnAction.java
+++ src/main/java/com/megacrit/cardcrawl/actions/common/MonsterStartTurnAction.java
@@ -1,34 +1,23 @@
-/*    */ package com.megacrit.cardcrawl.actions.common;
-/*    */ 
-/*    */
+package com.megacrit.cardcrawl.actions.common;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public class MonsterStartTurnAction
-/*    */   extends AbstractGameAction
-/*    */ {
-/* 12 */   private static final float DURATION = Settings.ACTION_DUR_FAST;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */   
-/*    */   public void update() {
-/* 20 */     if (this.duration == DURATION) {
-/* 21 */       this.isDone = true;
-/* 22 */       (AbstractDungeon.getCurrRoom()).monsters.applyPreTurnLogic();
-/*    */     } 
-/* 24 */     tickDuration();
-/*    */   }
-/*    */ }
 
+/* loaded from: desktop-1.0.jar:com/megacrit/cardcrawl/actions/common/MonsterStartTurnAction.class */
+public class MonsterStartTurnAction extends AbstractGameAction {
+    private static final float DURATION = Settings.ACTION_DUR_FAST;
 
-/* Location:              E:\代码\SlayTheSpire\desktop-1.0.jar!\com\megacrit\cardcrawl\actions\common\MonsterStartTurnAction.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
+    public MonsterStartTurnAction() {
+        this.duration = DURATION;
+    }
+
+    @Override // com.megacrit.cardcrawl.actions.AbstractGameAction
+    public void update() {
+        if (this.duration == DURATION) {
+            this.isDone = true;
+            AbstractDungeon.getCurrRoom().monsters.applyPreTurnLogic();
+        }
+        tickDuration();
+    }
+}
--- src/main/java/com/megacrit/cardcrawl/vfx/combat/HeartMegaDebuffEffect.java
+++ src/main/java/com/megacrit/cardcrawl/vfx/combat/HeartMegaDebuffEffect.java
@@ -1,57 +1,48 @@
-/*    */ package com.megacrit.cardcrawl.vfx.combat;
-/*    */ 
-/*    */
+package com.megacrit.cardcrawl.vfx.combat;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Interpolation;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public class HeartMegaDebuffEffect
-/*    */   extends AbstractGameEffect
-/*    */ {
-/*    */   public void update() {
-/* 23 */     if (this.duration == this.startingDuration) {
-/* 24 */       CardCrawlGame.sound.playA("GHOST_ORB_IGNITE_1", -0.6F);
-/*    */     }
-/* 26 */     if (this.duration > this.startingDuration / 2.0F) {
-/* 27 */       this.color.a = Interpolation.bounceIn.apply(1.0F, 0.0F, (this.duration - this.startingDuration / 2.0F) / this.startingDuration / 2.0F);
-/*    */     
-/*    */     }
-/*    */     else {
-/*    */       
-/* 32 */       this.color.a = Interpolation.bounceOut.apply(this.duration * this.startingDuration / 2.0F);
-/*    */     } 
-/* 34 */     this.duration -= Gdx.graphics.getDeltaTime();
-/* 35 */     if (this.duration < 0.0F) {
-/* 36 */       this.isDone = true;
-/*    */     }
-/*    */   }
-/*    */ 
-/*    */   
-/*    */   public void render(SpriteBatch sb) {
-/* 42 */     sb.setColor(new Color(0.0F, 0.0F, 0.0F, this.color.a * 0.8F));
-/* 43 */     sb.draw(ImageMaster.WHITE_SQUARE_IMG, 0.0F, 0.0F, Settings.WIDTH, Settings.HEIGHT);
-/* 44 */     sb.setColor(this.color);
-/* 45 */     sb.draw((TextureRegion)ImageMaster.BORDER_GLOW_2, 0.0F, 0.0F, Settings.WIDTH, Settings.HEIGHT);
-/*    */   }
-/*    */   
-/*    */   public void dispose() {}
-/*    */ }
 
+/* loaded from: desktop-1.0.jar:com/megacrit/cardcrawl/vfx/combat/HeartMegaDebuffEffect.class */
+public class HeartMegaDebuffEffect extends AbstractGameEffect {
+    public HeartMegaDebuffEffect() {
+        this.startingDuration = 4.0f;
+        this.duration = this.startingDuration;
+        this.color = new Color(0.9f, 0.7f, 1.0f, 0.0f);
+        this.renderBehind = true;
+    }
 
-/* Location:              E:\代码\SlayTheSpire\desktop-1.0.jar!\com\megacrit\cardcrawl\vfx\combat\HeartMegaDebuffEffect.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
+    @Override // com.megacrit.cardcrawl.vfx.AbstractGameEffect
+    public void update() {
+        if (this.duration == this.startingDuration) {
+            CardCrawlGame.sound.playA("GHOST_ORB_IGNITE_1", -0.6f);
+        }
+        if (this.duration > this.startingDuration / 2.0f) {
+            this.color.a = Interpolation.bounceIn.apply(1.0f, 0.0f, (this.duration - (this.startingDuration / 2.0f)) / (this.startingDuration / 2.0f));
+        } else {
+            this.color.a = Interpolation.bounceOut.apply(this.duration * (this.startingDuration / 2.0f));
+        }
+        this.duration -= Gdx.graphics.getDeltaTime();
+        if (this.duration < 0.0f) {
+            this.isDone = true;
+        }
+    }
+
+    @Override // com.megacrit.cardcrawl.vfx.AbstractGameEffect
+    public void render(SpriteBatch sb) {
+        sb.setColor(new Color(0.0f, 0.0f, 0.0f, this.color.a * 0.8f));
+        sb.draw(ImageMaster.WHITE_SQUARE_IMG, 0.0f, 0.0f, Settings.WIDTH, Settings.HEIGHT);
+        sb.setColor(this.color);
+        sb.draw(ImageMaster.BORDER_GLOW_2, 0.0f, 0.0f, Settings.WIDTH, Settings.HEIGHT);
+    }
+
+    @Override // com.megacrit.cardcrawl.vfx.AbstractGameEffect, com.badlogic.gdx.utils.Disposable
+    public void dispose() {
+    }
+}
