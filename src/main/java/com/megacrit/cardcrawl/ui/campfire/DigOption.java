41dff07b81d42d90ef637a2f95ad57e5db82389d
Li Binghui <1119604962@qq.com>
2025-10-04 23:46:36 +0800

    fix: 修复火堆有挖掘选项时崩溃的bug

--- src/main/java/com/megacrit/cardcrawl/ui/campfire/DigOption.java
+++ src/main/java/com/megacrit/cardcrawl/ui/campfire/DigOption.java
@@ -1,31 +1,24 @@
-/*    */ package com.megacrit.cardcrawl.ui.campfire;
-/*    */ 
-/*    */
+package com.megacrit.cardcrawl.ui.campfire;
 
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.vfx.campfire.CampfireDigEffect;
-/*    */ 
-/*    */ public class DigOption
-/*    */   extends AbstractCampfireOption {
-/* 10 */   private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("Dig Option");
-/* 11 */   public static final String[] TEXT = uiStrings.TEXT;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */   
-/*    */   public void useOption() {
-/* 21 */     AbstractDungeon.effectList.add(new CampfireDigEffect());
-/*    */   }
-/*    */ }
 
+/* loaded from: desktop-1.0.jar:com/megacrit/cardcrawl/ui/campfire/DigOption.class */
+public class DigOption extends AbstractCampfireOption {
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("Dig Option");
+    public static final String[] TEXT = uiStrings.TEXT;
 
-/* Location:              E:\代码\SlayTheSpire\desktop-1.0.jar!\com\megacrit\cardcraw\\ui\campfire\DigOption.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
+    public DigOption() {
+        this.label = TEXT[0];
+        this.description = TEXT[1];
+        this.img = ImageMaster.CAMPFIRE_DIG_BUTTON;
+    }
+
+    @Override // com.megacrit.cardcrawl.ui.campfire.AbstractCampfireOption
+    public void useOption() {
+        AbstractDungeon.effectList.add(new CampfireDigEffect());
+    }
+}
