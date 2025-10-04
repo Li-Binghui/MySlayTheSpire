b88eeddebf0a586e074afcfa120835b1672f08e0
Li Binghui <1119604962@qq.com>
2025-10-05 00:57:20 +0800

    fix: 修复神化打出后卡死的bug

--- src/main/java/com/megacrit/cardcrawl/actions/unique/ApotheosisAction.java
+++ src/main/java/com/megacrit/cardcrawl/actions/unique/ApotheosisAction.java
@@ -1,6 +1,4 @@
-/*    */ package com.megacrit.cardcrawl.actions.unique;
-/*    */ 
-/*    */
+package com.megacrit.cardcrawl.actions.unique;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
@@ -8,44 +6,38 @@ import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public class ApotheosisAction
-/*    */   extends AbstractGameAction
-/*    */ {
-/*    */   public void update() {
-/* 20 */     if (this.duration == Settings.ACTION_DUR_MED) {
-/* 21 */       AbstractPlayer p = AbstractDungeon.player;
-/*    */       
-/* 23 */       upgradeAllCardsInGroup(p.hand);
-/* 24 */       upgradeAllCardsInGroup(p.drawPile);
-/* 25 */       upgradeAllCardsInGroup(p.discardPile);
-/* 26 */       upgradeAllCardsInGroup(p.exhaustPile);
-/*    */       
-/* 28 */       this.isDone = true;
-/*    */     } 
-/*    */   }
-/*    */   
-/*    */   private void upgradeAllCardsInGroup(CardGroup cardGroup) {
-/* 33 */     for (AbstractCard c : cardGroup.group) {
-/* 34 */       if (c.canUpgrade()) {
-/* 35 */         if (cardGroup.type == CardGroup.CardGroupType.HAND) {
-/* 36 */           c.superFlash();
-/*    */         }
-/* 38 */         c.upgrade();
-/* 39 */         c.applyPowers();
-/*    */       } 
-/*    */     } 
-/*    */   }
-/*    */ }
+import java.util.Iterator;
 
+/* loaded from: desktop-1.0.jar:com/megacrit/cardcrawl/actions/unique/ApotheosisAction.class */
+public class ApotheosisAction extends AbstractGameAction {
+    public ApotheosisAction() {
+        this.duration = Settings.ACTION_DUR_MED;
+        this.actionType = AbstractGameAction.ActionType.WAIT;
+    }
 
-/* Location:              E:\代码\SlayTheSpire\desktop-1.0.jar!\com\megacrit\cardcrawl\action\\unique\ApotheosisAction.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
+    @Override // com.megacrit.cardcrawl.actions.AbstractGameAction
+    public void update() {
+        if (this.duration == Settings.ACTION_DUR_MED) {
+            AbstractPlayer p = AbstractDungeon.player;
+            upgradeAllCardsInGroup(p.hand);
+            upgradeAllCardsInGroup(p.drawPile);
+            upgradeAllCardsInGroup(p.discardPile);
+            upgradeAllCardsInGroup(p.exhaustPile);
+            this.isDone = true;
+        }
+    }
+
+    private void upgradeAllCardsInGroup(CardGroup cardGroup) {
+        Iterator<AbstractCard> it = cardGroup.group.iterator();
+        while (it.hasNext()) {
+            AbstractCard c = it.next();
+            if (c.canUpgrade()) {
+                if (cardGroup.type == CardGroup.CardGroupType.HAND) {
+                    c.superFlash();
+                }
+                c.upgrade();
+                c.applyPowers();
+            }
+        }
+    }
+}
