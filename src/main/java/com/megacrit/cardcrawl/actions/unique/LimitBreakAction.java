23a5aff8b203e3cf6d846288dade9eafefb660f0
Li Binghui <1119604962@qq.com>
2025-10-05 01:48:43 +0800

    fix: 修复突破极限打出后无效的问题

--- src/main/java/com/megacrit/cardcrawl/actions/unique/LimitBreakAction.java
+++ src/main/java/com/megacrit/cardcrawl/actions/unique/LimitBreakAction.java
@@ -1,38 +1,28 @@
-/*    */ package com.megacrit.cardcrawl.actions.unique;
-/*    */ 
-/*    */
+package com.megacrit.cardcrawl.actions.unique;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
-/*    */ 
-/*    */ public class LimitBreakAction
-/*    */   extends AbstractGameAction
-/*    */ {
-/* 15 */   private AbstractPlayer p = AbstractDungeon.player;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */   
-/*    */   public void update() {
-/* 21 */     if (this.duration == Settings.ACTION_DUR_XFAST && 
-/* 22 */       this.p.hasPower("Strength")) {
-/* 23 */       int strAmt = (this.p.getPower("Strength")).amount;
-/* 24 */       addToTop((AbstractGameAction)new ApplyPowerAction((AbstractCreature)this.p, (AbstractCreature)this.p, (AbstractPower)new StrengthPower((AbstractCreature)this.p, strAmt), strAmt));
-/*    */     } 
-/*    */ 
-/*    */     
-/* 28 */     tickDuration();
-/*    */   }
-/*    */ }
 
+/* loaded from: desktop-1.0.jar:com/megacrit/cardcrawl/actions/unique/LimitBreakAction.class */
+public class LimitBreakAction extends AbstractGameAction {
+    private AbstractPlayer p;
 
-/* Location:              E:\代码\SlayTheSpire\desktop-1.0.jar!\com\megacrit\cardcrawl\action\\unique\LimitBreakAction.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
+    public LimitBreakAction() {
+        this.actionType = AbstractGameAction.ActionType.WAIT;
+        this.p = AbstractDungeon.player;
+        this.duration = Settings.ACTION_DUR_XFAST;
+    }
+
+    @Override // com.megacrit.cardcrawl.actions.AbstractGameAction
+    public void update() {
+        if (this.duration == Settings.ACTION_DUR_XFAST && this.p.hasPower(StrengthPower.POWER_ID)) {
+            int strAmt = this.p.getPower(StrengthPower.POWER_ID).amount;
+            addToTop(new ApplyPowerAction(this.p, this.p, new StrengthPower(this.p, strAmt), strAmt));
+        }
+        tickDuration();
+    }
+}
