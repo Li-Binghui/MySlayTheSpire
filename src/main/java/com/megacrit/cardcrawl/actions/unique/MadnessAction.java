461e48f2499f70fc22ed852beb9faa7e87e49820
Li Binghui <1119604962@qq.com>
2025-10-05 01:01:41 +0800

    fix: 修复疯狂打出后无效的bug

--- src/main/java/com/megacrit/cardcrawl/actions/unique/MadnessAction.java
+++ src/main/java/com/megacrit/cardcrawl/actions/unique/MadnessAction.java
@@ -1,6 +1,4 @@
-/*    */ package com.megacrit.cardcrawl.actions.unique;
-/*    */ 
-/*    */
+package com.megacrit.cardcrawl.actions.unique;
 
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
@@ -8,65 +6,59 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public class MadnessAction
-/*    */   extends AbstractGameAction
-/*    */ {
-/* 15 */   private AbstractPlayer p = AbstractDungeon.player;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */   
-/*    */   public void update() {
-/* 21 */     if (this.duration == Settings.ACTION_DUR_FAST) {
-/* 22 */       boolean betterPossible = false;
-/* 23 */       boolean possible = false;
-/* 24 */       for (AbstractCard c : this.p.hand.group) {
-/* 25 */         if (c.costForTurn > 0) {
-/* 26 */           betterPossible = true; continue;
-/* 27 */         }  if (c.cost > 0) {
-/* 28 */           possible = true;
-/*    */         }
-/*    */       } 
-/* 31 */       if (betterPossible || possible) {
-/* 32 */         findAndModifyCard(betterPossible);
-/*    */       }
-/*    */     } 
-/*    */     
-/* 36 */     tickDuration();
-/*    */   }
-/*    */   
-/*    */   private void findAndModifyCard(boolean better) {
-/* 40 */     AbstractCard c = this.p.hand.getRandomCard(AbstractDungeon.cardRandomRng);
-/* 41 */     if (better) {
-/* 42 */       if (c.costForTurn > 0) {
-/*    */         
-/* 44 */         c.cost = 0;
-/* 45 */         c.costForTurn = 0;
-/* 46 */         c.isCostModified = true;
-/* 47 */         c.superFlash(Color.GOLD.cpy());
-/*    */       } else {
-/*    */         
-/* 50 */         findAndModifyCard(better);
-/*    */       }
-/*    */     
-/* 53 */     } else if (c.cost > 0) {
-/*    */       
-/* 55 */       c.cost = 0;
-/* 56 */       c.costForTurn = 0;
-/* 57 */       c.isCostModified = true;
-/* 58 */       c.superFlash(Color.GOLD.cpy());
-/*    */     } else {
-/*    */       
-/* 61 */       findAndModifyCard(better);
-/*    */     } 
-/*    */   }
-/*    */ }
+import java.util.Iterator;
 
+/* loaded from: desktop-1.0.jar:com/megacrit/cardcrawl/actions/unique/MadnessAction.class */
+public class MadnessAction extends AbstractGameAction {
+    private AbstractPlayer p;
 
-/* Location:              E:\代码\SlayTheSpire\desktop-1.0.jar!\com\megacrit\cardcrawl\action\\unique\MadnessAction.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
+    public MadnessAction() {
+        this.actionType = AbstractGameAction.ActionType.CARD_MANIPULATION;
+        this.p = AbstractDungeon.player;
+        this.duration = Settings.ACTION_DUR_FAST;
+    }
+
+    @Override // com.megacrit.cardcrawl.actions.AbstractGameAction
+    public void update() {
+        if (this.duration == Settings.ACTION_DUR_FAST) {
+            boolean betterPossible = false;
+            boolean possible = false;
+            Iterator<AbstractCard> it = this.p.hand.group.iterator();
+            while (it.hasNext()) {
+                AbstractCard c = it.next();
+                if (c.costForTurn > 0) {
+                    betterPossible = true;
+                } else if (c.cost > 0) {
+                    possible = true;
+                }
+            }
+            if (betterPossible || possible) {
+                findAndModifyCard(betterPossible);
+            }
+        }
+        tickDuration();
+    }
+
+    private void findAndModifyCard(boolean better) {
+        AbstractCard c = this.p.hand.getRandomCard(AbstractDungeon.cardRandomRng);
+        if (better) {
+            if (c.costForTurn > 0) {
+                c.cost = 0;
+                c.costForTurn = 0;
+                c.isCostModified = true;
+                c.superFlash(Color.GOLD.cpy());
+                return;
+            }
+            findAndModifyCard(better);
+            return;
+        }
+        if (c.cost > 0) {
+            c.cost = 0;
+            c.costForTurn = 0;
+            c.isCostModified = true;
+            c.superFlash(Color.GOLD.cpy());
+            return;
+        }
+        findAndModifyCard(better);
+    }
+}
